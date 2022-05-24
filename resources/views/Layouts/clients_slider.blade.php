672bb7a6baae7d3375aaceaf1c2a010735cbde4c
Renatus John <kinate82@gmail.com>
2022-05-24 11:13:28 +0300

    Fix: Add clients image on home client slide section and clients folder with clients logos

--- resources/views/Layouts/clients_slider.blade.php
+++ resources/views/Layouts/clients_slider.blade.php
@@ -9,13 +9,14 @@
       <div class="space space-sm"></div>
       <!-- row of sliders-->
    <section class="customer-logos slider">
-      <div class="slide"><img src="https://tiob.hellencp.com/img/tiob_logo.png"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
+      <div class="slide"><img src="https://tiob.hellencp.com/img/tiob_logo.png"  style="height:90px; width:auto"></div>
+      <div class="slide"><img src="assets/images/clients/aga.svg"></div>
+      <div class="slide"><img src="assets/images/clients/abc_logo.png" style="height:90px; width:auto"></div>
+      <div class="slide"><img src="assets/images/clients/anazak.png"  style="height:90px; width:auto"></div>
+      <div class="slide"><img src="assets/images/clients/eminence.svg"></div>
+      <div class="slide"><img src="assets/images/clients/endoxa.svg"></div>
+      <div class="slide"><img src="assets/images/clients/mapacha.png"></div>
+      <div class="slide"><img src="assets/images/clients/waziri.png"></div>
    </section>
     </div>
   </div>
